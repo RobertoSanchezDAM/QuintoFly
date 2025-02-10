6b7dc747f41246bb6ba07658b2bf3973591c15dc
roberto <roberto@wiqrumedia.com>
2025-02-10 10:30:43 +0100

    workflow añadido (no sale la pestaña, arreglar)

--- models/vuelo.py
+++ models/vuelo.py
@@ -11,12 +11,12 @@ class Vuelo(models.Model):
     origen = fields.Char("Origen", required=True)
     aeronave_id = fields.Many2one('quintofly.aeronave', string="Aeronave", required=True)
     destino = fields.Char("Destino", required=True)
-    fecha_vuelo = fields.Datetime("Fecha del vuelo", required=True)
-
+    fecha_salida = fields.Datetime("Fecha y hora de salida", required=True)
+    fecha_llegada = fields.Datetime("Fecha y hora de llegada", required=True)
 
     def name_get(self):
         result = []
         for record in self:
-            name = f"Vuelo {record.id} --- {record.origen} - {record.destino} --- {record.fecha_vuelo}"
+            name = f"Vuelo {record.id} --- {record.origen} - {record.destino} --- {record.fecha_salida}"
             result.append((record.id, name))
         return result
